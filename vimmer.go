14abcb4a2e8b99300a10b94daebfe450ea59753a
David Banham <david@banham.id.au>
2013-11-15 14:12:19 +1100

    fixed conflict between mvimmer and vimmer

--- vimmer.go
+++ vimmer.go
@@ -2,18 +2,39 @@ package main
 
 import (
   "os"
-  "os/exec"
   "fmt"
-  "log"
   "bufio"
+  "os/exec"
+  "log"
 )
 func main() {
-  files := os.Args[1:]
-  if len(files) == 0 {
+  args := os.Args[1:]
+  if len(args) == 0 {
     fmt.Println("Provide your files as a list of arguments.")
     fmt.Println("eg: ls ./* | xargs vimmer")
+    fmt.Println("There is an optional -m flag to run mvim instead of vim")
     return
   }
+  if args[0] == "-m" {
+    mvim(args[1:])
+  } else {
+    vim(args[0:])
+  }
+
+}
+func check_if_continue() {
+  fmt.Println("Enter to continue, anything else to bail")
+  reader := bufio.NewReader(os.Stdin)
+  resp, err := reader.ReadString('\n')
+  if resp != "\n" {
+    log.Fatal("Got something other than enter: ", resp)
+  }
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func vim(files []string) {
   for i := 0 ; i < len(files) ; i++ {
     cmd := exec.Command("vim", files[i])
     cmd.Stdin = os.Stdin
@@ -29,14 +50,13 @@ func main() {
   }
 }
 
-func check_if_continue() {
-  fmt.Println("Enter to continue, anything else to bail")
-  reader := bufio.NewReader(os.Stdin)
-  resp, err := reader.ReadString('\n')
-  if resp != "\n" {
-    log.Fatal("Got something other than enter: ", resp)
-  }
-  if err != nil {
-    log.Fatal(err)
+func mvim(files []string) {
+  for i := 0 ; i < len(files) ; i++ {
+    cmd := exec.Command("mvim", files[i])
+    err := cmd.Run()
+    if err != nil {
+      log.Fatal(err)
+    }
   }
 }
+
